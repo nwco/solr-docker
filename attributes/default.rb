e8d54b0b13cd6bec33d659933adef7343b47f58a
Nick White <nickwhite44@gmail.com>
2017-08-12 20:49:28 -0600

    Modified motd template to display Solr and Java info.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,8 @@
 default['solr-docker']['message'] = 'A wonderful day for Solr!'
+default['solr-docker']['java_home'] = '/usr/lib/jvm/java'
+default['solr-docker']['java_bin'] = '/usr/lib/jvm/java/bin/java'
+default['solr-docker']['solr_port'] = '8983'
+default['solr-docker']['solr_version'] = '4.10.3 (upstream)'
 # 'java' cookbook attributes
 default['java']['install_flavor'] = 'oracle'
 default['java']['jdk_version'] = '8'
